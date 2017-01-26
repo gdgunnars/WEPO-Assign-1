ed62488cc0233c94291fee1dfba4d39076f2d504
Hlynur Stefánsson <hlynurs15@ru.is>
2017-01-26 18:25:03 +0000

    Add circle draw

--- Script.js
+++ Script.js
@@ -2,3 +2,20 @@
 
 var canvas = document.getElementById("mainCanvas");
 var context = canvas.getContext("2d");
+
+$('input[type=radio][name=shape]').on('change', function() {
+    switch($(this).val()) {
+        case 'Rectangle':
+            console.log("Set shape to rectangle");
+            setShape("Rectangle");
+            break;
+        case 'Pen':
+            console.log("Set shape to Pen");
+            setShape("Pen");
+            break;
+        case 'Circle':
+            console.log("Set shape to Circle");
+            setShape("Circle");
+            break;
+    }
+});
--- scripts/circle.js
+++ scripts/circle.js
@@ -4,9 +4,27 @@ class Circle extends Shape {
     }
 
     draw(context) {
+        context.strokeStyle = this.color;
+
+        var radiusX = (this.x - this.endX) * 0.5,
+            radiusY = (this.y - this.endY) * 0.5,
+            centerX = this.x - radiusX,
+            centerY = this.y - radiusY,
+            step = 0.01,
+            a = step,
+            pi2 = Math.PI * 2 - step;
+
         context.beginPath();
-        context.lineWidth = this.lineWidth;
-        context.arc(this.x,this.y,50,0,2*Math.PI);
+        context.moveTo(centerX + radiusX * Math.cos(0),
+                       centerY + radiusY * Math.sin(0));
+
+        for(; a < pi2; a += step) {
+            context.lineTo(centerX + radiusX * Math.cos(a),
+                           centerY + radiusY * Math.sin(a));
+        }
+
         context.stroke();
+        context.closePath();
+
     }
 }
